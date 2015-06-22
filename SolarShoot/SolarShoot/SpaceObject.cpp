fb202c30f7a68765e6a312dc0a0867df2e76cb3c
Namtar <matthias.drummer@gmail.com>
2015-06-22 17:01:26 +0200

    ufo finally working

--- SolarShoot/SolarShoot/SpaceObject.cpp
+++ SolarShoot/SolarShoot/SpaceObject.cpp
@@ -14,6 +14,7 @@ SpaceObject::SpaceObject(GLuint programId, char* textureFileName, char* objectFi
 SpaceObject::~SpaceObject(void)
 {
 	// TODO: cleanups
+	// glDeleteBuffers
 }
 
 void SpaceObject::setupBuffers() 
